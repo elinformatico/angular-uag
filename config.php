fc8d1522e55a22b0533242af8748b3d2e5eb4336
jnhernandez <jnhernandez@tracsa.com.mx>
2016-04-28 17:47:29 -0500

    Incluimos todos los Plugins de Javascript en el Documento HTML principal

---
+++ config.php
@@ -0,0 +1,7 @@
+<?php
+	
+	define('APP_BASE', 'http://localhost/mobie.site');
+	define('VENDOR_JS', APP_BASE . '/js/vendor');
+
+	$global['app_base'] = APP_BASE;
+	$global['vendor_js'] = VENDOR_JS;
--- index.php
+++ index.php
@@ -1,17 +1,4 @@
-<!DOCTYPE html>
-<html>
-<head>
-    <title>Mobie - Find the Best Movie</title>
-     <meta http-equiv="Content-Type" content="text/html" charset="utf-8" />
-     <meta name="author" content="Jose Noe Hernandez Vivanco"/>
-     <meta name="description" content="Find the best Movie"/>
-     <meta name="keywords" content="Movies, Mobie"/>
-     <meta http-equiv="X-UA-Compatible" content="IE=9" />
-     <meta name="viewport" content="initial-scale=1, maximum-scale=1">
-</head>
-<body>  
-
-    
-
-</body>
-</html>
+<?php
+    # Boostraping Init
+    include_once 'config.php';
+    include_once 'init.php';
