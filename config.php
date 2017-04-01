4e8540075f6a9a493957549bba27f2023a7f7c89
J.Noe Hernandez Vivanco <donoe1985@gmail.com>
2017-04-01 13:02:46 -0400

    Configuramos para que funcione en Produccion

--- config.php
+++ config.php
@@ -3,7 +3,8 @@
 	# define('TEMPLATE', 'prueba');
 	define('TEMPLATE', 'main');
 
-	define('APP_BASE', 'http://localhost/mobie.site');
+	//define('APP_BASE', 'http://localhost/mobie.site');
+        define('APP_BASE', 'http://noehdez.info/site');
 	define('JS_PATH', APP_BASE . '/js');
 	define('CSS_PATH', APP_BASE . '/css');
 
--- js/app.js
+++ js/app.js
@@ -1,11 +1,11 @@
 angular.module('mobieApp', ['ui.bootstrap'])
 .constant('globalServerRoute', {
 
-	apiRoute  : 'http://localhost/mobie.mx/public/mobie', 
-	siteRoute : 'http://localhost/mobie.site',
+	//apiRoute  : 'http://localhost/mobie.mx/public/mobie', 
+	//siteRoute : 'http://localhost/mobie.site',
 
-	// apiRoute  : 'http://noehdez.info/api/public/mobie',
-    // 	siteRoute : 'http://noehdez.info/site'
+	 apiRoute  : 'http://noehdez.info/api/public/mobie',
+         siteRoute : 'http://noehdez.info/site'
 })
 .run(function($rootScope, globalServerRoute){
 	$rootScope.api  = globalServerRoute.apiRoute;
