3e682ac098d5c35bf34969dd09edff1957cd6d5a
J.Noe Hernandez Vivanco <donoe1985@gmail.com>
2017-07-04 20:39:34 -0400

    fix: Adding https to the links

--- config.php
+++ config.php
@@ -12,7 +12,7 @@
 	$app_base = strstr($_SERVER['HTTP_HOST'], 'localhost') ? 
 
 	'http://localhost/uag/mobie.site' :    # DEVELOPMENT
-	'http://noehdez.info/site';            # PRODUCTION
+	'https://noehdez.info/site';            # PRODUCTION
 
 	define('APP_BASE', $app_base);
 	define('JS_PATH', APP_BASE . '/js');
--- templates/main-uag.php
+++ templates/main-uag.php
@@ -12,11 +12,11 @@
   <script src="<?php echo $global['app_base']; ?>/js/vendor/angular/angular.min.js"></script>
   <!-- ================================================================================= -->
 
-  <link rel="stylesheet" href="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css">
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css">
   <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.0/jquery.min.js"></script>
-  <script src="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js"></script>
+  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js"></script>
 
-  <link rel="stylesheet" href="//maxcdn.bootstrapcdn.com/font-awesome/4.3.0/css/font-awesome.min.css">
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.3.0/css/font-awesome.min.css">
   
   
   <script src="<?php echo $global['app_base']; ?>/js/vendor/animate/angular-animate.min.js"></script>
